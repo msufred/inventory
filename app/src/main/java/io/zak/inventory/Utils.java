2220cc245c1e1ae850451d8c4b53bc3c7ee2702d
Gem Seeker <gemseeker24@gmail.com>
2025-02-24 21:36:35 +0800

    added toStringMoneyFormat method

--- app/src/main/java/io/zak/inventory/Utils.java
+++ app/src/main/java/io/zak/inventory/Utils.java
@@ -3,6 +3,8 @@ package io.zak.inventory;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Locale;
+
 public class Utils {
 
     /**
@@ -12,6 +14,37 @@ public class Utils {
      */
     public static String normalize(String str) {
         return str.replace("'", "`");
+    }
+
+    /**
+     * Convert double value to String money format. Example, 1000.00 is converted to 10,000.00.
+     * @param amount amount to be converted to a String money format
+     * @return String money format
+     */
+    public static String toStringMoneyFormat(double amount) {
+        String str = String.format(Locale.getDefault(), "%.2f", Math.abs(amount));
+        StringBuilder sb = new StringBuilder();
+        int startIndex = str.indexOf('.');
+        if (startIndex < 0) startIndex = 0;
+        int decimalCount = 0;
+        for (int i = startIndex - 1; i >= 0; i--) {
+            if (decimalCount == 3) {
+                sb.append(',');
+                decimalCount = 0;
+            }
+            sb.append(str.charAt(i));
+            decimalCount++;
+        }
+        String output = "";
+        if (amount < 0) {
+            output = "-";
+        }
+        output += sb.reverse().append(str.substring(startIndex)).toString();
+        // if output is like `-,500.00`, remove first two characters
+        if (output.charAt(0) == '-' && output.charAt(1) == ',') {
+            output = output.substring(2);
+        }
+        return output;
     }
 
     /**
@@ -36,6 +69,10 @@ public class Utils {
         return sp.getInt("user_id", -1);
     }
 
+    /**
+     * Logout User by removing user id in SharedPreferences.
+     * @param context Context
+     */
     public static void logout(Context context) {
         SharedPreferences sp = context.getSharedPreferences("login", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
