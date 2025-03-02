fa9e30165d990934455c7872eafdcb3d1a84112f
Gem Seeker <gemseeker24@gmail.com>
2025-03-02 10:02:49 +0800

    added isTheSameDate() method

--- app/src/main/java/io/zak/inventory/Utils.java
+++ app/src/main/java/io/zak/inventory/Utils.java
@@ -84,6 +84,18 @@ public class Utils {
         }
     }
 
+    public boolean isTheSameDate(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+
+        return (cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH)) &&
+                (cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH)) &&
+                (cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR));
+    }
+
     /**
      * Save User id to SharedPreferences.
      * @param context Context
