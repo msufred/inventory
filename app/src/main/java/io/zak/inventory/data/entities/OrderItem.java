0d90b57a79061507a7b1e1c0ccd6b73a2a279747
Gem Seeker <gemseeker24@gmail.com>
2025-03-05 06:29:41 +0800

    removed fkVehicleId; aded sellingPrice

--- app/src/main/java/io/zak/inventory/data/entities/OrderItem.java
+++ app/src/main/java/io/zak/inventory/data/entities/OrderItem.java
@@ -4,6 +4,9 @@ import androidx.room.Entity;
 import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
+/**
+ * Represents an item in an order transaction.
+ */
 @Entity(tableName = "order_items", foreignKeys = {
         @ForeignKey(entity = Order.class, parentColumns = "orderId", childColumns = "fkOrderId", onDelete = ForeignKey.CASCADE),
         @ForeignKey(entity = Product.class, parentColumns = "productId", childColumns = "fkProductId", onDelete = ForeignKey.CASCADE),
@@ -14,8 +17,8 @@ public class OrderItem {
     @PrimaryKey
     public int orderItemId;
     public int fkOrderId;
-    public int fkProductId;
-    public int fkVehicleStockId;
+    public int fkProductId; // assume inventory and delivery has the same product entry
+    public double sellingPrice;
     public int quantity;
     public double subtotal;
 }
