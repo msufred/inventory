d469f1288447bf8e111aa1f195569933a66b86b7
Gem Seeker <gemseeker24@gmail.com>
2025-03-07 06:41:46 +0800

    added fkWarehouseStockId

--- app/src/main/java/io/zak/inventory/data/entities/OrderItem.java
+++ app/src/main/java/io/zak/inventory/data/entities/OrderItem.java
@@ -9,15 +9,16 @@ import androidx.room.PrimaryKey;
  */
 @Entity(tableName = "order_items", foreignKeys = {
         @ForeignKey(entity = Order.class, parentColumns = "orderId", childColumns = "fkOrderId", onDelete = ForeignKey.CASCADE),
-        @ForeignKey(entity = Product.class, parentColumns = "productId", childColumns = "fkProductId", onDelete = ForeignKey.CASCADE),
-        @ForeignKey(entity = VehicleStock.class, parentColumns = "vehicleStockId", childColumns = "fkVehicleStockId")
+        @ForeignKey(entity = WarehouseStock.class, parentColumns = "warehouseStockId", childColumns = "fkWarehouseStockId", onDelete = ForeignKey.CASCADE),
+        @ForeignKey(entity = Product.class, parentColumns = "productId", childColumns = "fkProductId", onDelete = ForeignKey.CASCADE)
 })
 public class OrderItem {
 
     @PrimaryKey
-    public int orderItemId;
-    public int fkOrderId;
-    public int fkProductId; // assume inventory and delivery has the same product entry
+    public int orderItemId;         // id is based on the OrderItem ID of the Delivery App
+    public int fkOrderId;           // the ID of the Order this item belongs to
+    public int fkWarehouseStockId;  // WarehouseStock ID of the product
+    public int fkProductId;         // Product ID; assumes Delivery App and Inventory App shares the same database
     public double sellingPrice;
     public int quantity;
     public double subtotal;
