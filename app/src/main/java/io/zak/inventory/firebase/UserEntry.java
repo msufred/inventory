28154306baf4a08e979bcc8112100c0ffd588f42
Gem Seeker <gemseeker24@gmail.com>
2025-03-15 09:51:38 +0800

    added license, uid, and vehicleId

--- app/src/main/java/io/zak/inventory/firebase/UserEntry.java
+++ app/src/main/java/io/zak/inventory/firebase/UserEntry.java
@@ -1,14 +1,20 @@
 package io.zak.inventory.firebase;
 
+import com.google.firebase.database.Exclude;
 import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class UserEntry {
+    @Exclude
+    public String uid;
+
     public String fullName;
     public String position;
     public String address;
     public String email;
     public String contactNo;
+    public String license;
+    public int vehicleId;
 
     public UserEntry() {
         // required empty constructor
