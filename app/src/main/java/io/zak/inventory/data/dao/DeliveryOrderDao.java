6ff7c3b1b372691f914a6c42c0f01cb24febb959
Gem Seeker <gemseeker24@gmail.com>
2025-03-15 09:49:55 +0800

    removed queries

--- app/src/main/java/io/zak/inventory/data/dao/DeliveryOrderDao.java
+++ app/src/main/java/io/zak/inventory/data/dao/DeliveryOrderDao.java
@@ -9,7 +9,6 @@ import androidx.room.Update;
 import java.util.List;
 
 import io.zak.inventory.data.entities.DeliveryOrder;
-import io.zak.inventory.data.relations.DeliveryDetails;
 
 @Dao
 public interface DeliveryOrderDao {
@@ -32,17 +31,4 @@ public interface DeliveryOrderDao {
     @Query("SELECT COUNT(*) FROM delivery_orders")
     int getSize();
 
-    // Custom queries
-    @Query("SELECT delivery_orders.*, vehicles.*, employees.* " +
-            "FROM delivery_orders " +
-            "INNER JOIN vehicles ON delivery_orders.fkVehicleId=vehicles.vehicleId " +
-            "INNER JOIN employees ON delivery_orders.fkEmployeeId=employees.employeeId")
-    List<DeliveryDetails> getDeliveryOrdersWithDetails();
-
-    @Query("SELECT delivery_orders.*, vehicles.*, employees.* " +
-            "FROM delivery_orders " +
-            "INNER JOIN vehicles ON delivery_orders.fkVehicleId=vehicles.vehicleId " +
-            "INNER JOIN employees ON delivery_orders.fkEmployeeId=employees.employeeId " +
-            "WHERE delivery_orders.deliveryOrderId=:deliveryOrderId")
-    List<DeliveryDetails> getDeliveryOrderDetails(int deliveryOrderId);
 }
