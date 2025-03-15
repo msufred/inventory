1d14ff5c0c122b5d70603eda6c872de8b0367f3f
Gem Seeker <gemseeker24@gmail.com>
2025-03-15 10:04:35 +0800

    added vehicleName and plateNo

--- app/src/main/java/io/zak/inventory/firebase/AssignedVehicleEntry.java
+++ app/src/main/java/io/zak/inventory/firebase/AssignedVehicleEntry.java
@@ -7,13 +7,17 @@ public class AssignedVehicleEntry {
 
     public String userId;
     public int vehicleId;
+    public String vehicleName;
+    public String plateNo;
 
     public AssignedVehicleEntry() {
         // empty
     }
 
-    public AssignedVehicleEntry(String uid, int vehicleId) {
+    public AssignedVehicleEntry(String uid, int vehicleId, String vehicleName, String plateNo) {
         this.userId = uid;
         this.vehicleId = vehicleId;
+        this.vehicleName = vehicleName;
+        this.plateNo = plateNo;
     }
 }
